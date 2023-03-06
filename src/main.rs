778c3281faed19e4424abe0b404520fdd48659d6
aalhendi <abdulrazaq.alhendi@gmail.com>
2023-03-06 04:18:07 +0300

    init: windows platform hello world

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
-fn main() {
-    println!("Hello, world!");
+use windows::{core::*, Win32::Foundation::*, Win32::UI::WindowsAndMessaging::*};
+
+fn main() -> Result<()> {
+    unsafe {
+        MessageBoxA(
+            HWND(0),
+            s!("Hello, World."),
+            s!("lolping"),
+            MB_OK | MB_ICONINFORMATION,
+        );
+    }
+
+    Ok(())
 }
